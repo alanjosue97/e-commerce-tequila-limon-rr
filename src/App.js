36cc0727ddbb212d1e538b3379f9d2ddbe751bb2
alanjosue97 <perezaln.09@gmail.com>
2023-01-19 23:22:14 -0600

    Be its done all routers, was add secction navbar and footer secction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
 import {
   RouterProvider,
-} from "react-router-dom";
+  } from "react-router-dom";
 import {router} from './router'
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,46 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter,Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
+import Navbar from "../Components/Navbar/Navbar";
+import Home from "../page/home/Home";
+import Product from "../page/Product/Product";
+import Products from "../page/Products/Products";
 
+
+
+const Layout = () =>{
+    return(
+      <div className='App'>
+      <Footer/>
+      <Outlet/>
+      <Navbar/>
+     
+      </div>
+    )
+    
+  }
 export const router = createBrowserRouter ([
     {
         path: "/",
-        element: <span>home</span>
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+                
+            },
+            {
+                path: "/products/:id",
+                element: <Products/>
+                
+            },
+            {
+                path: "/product/:id",
+                element: <Product/>
+                
+            }
+
+        ]
         
     },
-    {
-        path: "/products/:id",
-        element: <span>Category</span>
-        
-    },
-    {
-        path: "/product/:id",
-        element: <span>Product</span>
-        
-    }
+   
 ])
